8dabb200d942b5a5a548a77dfa6872c6ea6eb183
Bikash <bikash13763@gmail.com>
2026-07-25 10:50:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://prepara-eight.vercel.app', 'https://prepara.bksh.site'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://prepara-eight.vercel.app', 'https://prepara.bksh.site', 'https://prepara.useiota.space'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: [
